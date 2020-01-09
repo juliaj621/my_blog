c60df7ecc5a02849104a78ea938a65016db9a7ee
Julia Jachimowicz <juliaj621@gmail.com>
2020-01-09 13:44:10 -0800

    update server and add views

--- express_server.js
+++ express_server.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const app = express();
+const partials = require('express-partials');
 const PORT = 8080; // default port 8080
 
-app.get("/", (req, res) => {
-  res.send("Main Page!");
-});
+app.set('view engine', 'ejs');
+// Include partials middleware into the server
+app.use(partials());
 
-app.get("/about", (req, res) => {
-  res.send("About me!");
-});
-
-app.get("/socialmedia", (req, res) => {
-  res.send("social media!");
-});
-
-app.get("/github", (req, res) => {
-  res.send("github!");
-});
-
-app.get("/resume", (req, res) => {
-  res.send("Resume!");
-});
-
+app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/socialmedia", (req, res) => {
+  res.render("social_media");
+});
+
+app.get("/github", (req, res) => {
+  res.render("github");
+});
+
+app.get("/resume", (req, res) => {
+  res.render("resume");
+});
+
