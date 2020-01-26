53c6e50bd142ba18b0a9b8066bb34d43355c5e55
Julia Jachimowicz <juliaj621@gmail.com>
2020-01-25 16:56:07 -0800

    incorporating views

--- src/api.js
+++ src/api.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const partials = require('express-partials');
 
 const app = express();
 
 const router = express.Router();
 
+app.engine('.html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/dist');
+// Include partials middleware into the server
+app.use(partials());
+
 router.get("/", (req, res) => {
-  res.json({
-    'hello': "hi"
-  })
+  res.render("index")
 });
+
+router.get("/about", (req, res) => {
+  res.render("about")
+})
 
 app.use("/.netlify/functions/api",  router);
 
